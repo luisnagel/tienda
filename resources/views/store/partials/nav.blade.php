41f40164bc7991fc1ca3286623628e9f1e89ea11
Luis Nagel <luist.nagel@gmail.com>
2017-01-27 10:19:19 -0300

    inicial

--- resources/views/store/partials/nav.blade.php
+++ resources/views/store/partials/nav.blade.php
@@ -11,7 +11,7 @@
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <p class="navbar-text">My Laravel Store</p>
+      <p class="navbar-text">La re tienda!!</p>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="{{ route('cart-show') }}"><i class="fa fa-shopping-cart"></i></a></li>
         <li><a href="#">Conocenos</a></li>
